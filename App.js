b6b0537c703c4f0df5fb1d08ae5fd5023389910e
Stefano Ramirez <stderami@honeygrid.ai>
2023-05-30 15:18:51 -0600

    add main with some native components as to test react-native and expo.

--- App.js
+++ App.js
@@ -1,20 +1,75 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Image, ScrollView } from 'react-native';
+import React from 'react';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+    <ScrollView contentContainerStyle={styles.container}>
+      <Text style={styles.heading}>React Native Components Demo</Text>
+
+      <Text style={styles.label}>Text Input:</Text>
+      <TextInput style={styles.input} placeholder="Enter text" />
+
+      <Text style={styles.label}>Button:</Text>
+      <Button title="Press me" onPress={() => alert('Button pressed!')} />
+
+      <Text style={styles.label}>Image:</Text>
+      <Image
+          source={{
+            uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
+          }}
+          style={{width: 200, height: 200}}
+        />
+
+      <Text style={styles.label}>ScrollView:</Text>
+      <ScrollView style={styles.scrollView}>
+        <Text style={styles.scrollViewText}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ut sem at mi euismod pretium at nec mauris.
+          In consectetur auctor aliquet. Integer nec eros dolor. Aliquam erat volutpat. Nullam pellentesque lacinia
+          velit ut luctus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;
+          Etiam consectetur felis in sapien finibus, nec interdum lacus interdum. Sed ullamcorper libero at ultricies
+          rhoncus. Vestibulum mattis luctus enim quis volutpat. Ut eleifend est nec orci ultricies, sed elementum
+          lacus condimentum.
+        </Text>
+      </ScrollView>
+
       <StatusBar style="auto" />
-    </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 20,
+  },
+  heading: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  label: {
+    fontSize: 18,
+    marginTop: 20,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    padding: 10,
+    marginBottom: 20,
+  },
+  image: {
+    width: 200,
+    height: 200,
+    marginBottom: 20,
+  },
+  scrollView: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    maxHeight: 200,
+    marginBottom: 20,
+  },
+  scrollViewText: {
+    padding: 10,
   },
 });
